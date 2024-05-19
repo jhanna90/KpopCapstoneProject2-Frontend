45f7480f1268e209e354b2f576df82eb87d8cc57
Josh <111018742+jhanna90@users.noreply.github.com>
2024-05-19 17:18:22 -0500

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_API_URL = "http://localhost:3001/api";
+const BASE_API_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001/api";
 
 // const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
